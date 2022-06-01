3f7e52aba7fd5c8e1af2e5ebaee75503e595293b
--help <20mm573@esisalama.org>
2022-06-01 17:58:22 +0200

    essais sur enregistrement du docteur

---
+++ lib/docteurs.py
@@ -0,0 +1,22 @@
+from lib.clean import*
+def enregistrer_docteur():
+    liste = []
+    print("Entrez le prenom du docteur")
+    prenom = input(">> ")
+    liste.append(prenom)
+    print("Entrez le nom du docteur")
+    nom = input(">> ")
+    liste.append(nom)
+    print("Entrez le post_nom du docteur")
+    post_nom = input(">> ")
+    liste.append(post_nom)
+    print("Entrez le telephone du docteur")
+    telephone = input(">> ")
+    liste.append(telephone)
+    print("Entrez le matricule du docteur")
+    matricule = input(">> ")
+    liste.append(matricule)
+    print("Entrez la specialite du docteur")
+    specialite = input(">> ")
+    liste.append(specialite)
+    
